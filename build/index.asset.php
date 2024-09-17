7ec90e148d6d938f22693ca36a105c56ed22ec00
Code Pixelz Media <arpanadhikari63@gmail.com>
2024-09-17 17:05:03 +0545

    conditional rendering of card blocks for different categories

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b392b54c3541b832167f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'a02a686ed0078c2d36db');
--- card-block.php
+++ card-block.php
@@ -35,3 +35,42 @@ add_action('init', 'create_block_card_block_block_init');
 // require_once __DIR__ . '/src/render.php';?
 
 
+function swiper_slide_enqueue_script()
+{
+	// Enqueue Swiper CSS
+	wp_enqueue_style(
+		'swiper-style',
+		'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css',
+		array(),
+		'8.0.7' // Swiper version
+	);
+
+	// Enqueue Swiper JS
+	wp_enqueue_script(
+		'swiper-script',
+		'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js',
+		array('jquery'), // Add jQuery as a dependency if needed
+		'8.0.7', // Swiper version
+		true // Load in footer
+	);
+}
+add_action('wp_enqueue_scripts', 'swiper_slide_enqueue_script');
+
+
+
+
+
+function card_block_register_block_styles()
+{
+	// Enqueue custom style for the 'card-block' block from theme
+	wp_enqueue_block_style(
+		'create-block/card-block', // Block name: namespace/block-name (ensure this matches your block)
+		array(
+			'handle' => 'card-block-style', // Unique handle for the style
+			'src' => get_template_directory_uri() . '/assets/public/css/wstr_card_block_style.css', // Path to the CSS file in your theme
+			'path' => get_template_directory() . '/assets/css/card-style.css', // Path to file for versioning
+			'ver' => filemtime(get_template_directory() . '/assets/css/card-style.css'), // Version based on file modification time
+		)
+	);
+}
+add_action('init', 'card_block_register_block_styles');
